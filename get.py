32fef15784d971511661976259a6f56377a37d1f
getcerts1 <barrowtech1999@gmail.com>
2025-02-15 14:42:16 +0300

    matching local with remote

--- get.py
+++ get.py
@@ -1,13 +1,12 @@
 import json
-
-from fastapi import FastAPI
-
+from fastapi import FastAPI, status, HTTPException
+from main2 import PostSchema
 
 FILE_PATH = "user_posts.json"
 app = FastAPI()
 
 
-@app.get("/get_user_info/{id}")
+@app.get("/get_user_info/{id}", status_code=status.HTTP_200_OK)
 async def get_info(id:str):
     try:
         with open(FILE_PATH, "r") as file:
@@ -17,8 +16,7 @@ async def get_info(id:str):
                     return post
             return {"Message":"Post not found"}
     except FileNotFoundError:
-        return {"Message":f"file does not exist"}
-
+        return {"Message":f"file does not exist"}, status.HTTP_404_NOT_FOUND
 
 
 @app.delete("/delete_post/{id}")
@@ -44,4 +42,35 @@ async def delete(id:str):
             return {"Message":"Deleted post successfully"}
 
     except FileNotFoundError:
-        return {"Message":f"file does not exist"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=
+                            "file does not exist")
+
+@app.put("/edit_post/{id}")
+async def put(id:str, post: PostSchema):
+    try:
+        with open(FILE_PATH, "r") as file:
+            try:
+                output = json.load(file)
+                if not isinstance(output, list):
+                    output = []
+            except json.JSONDecodeError:
+                output = []
+
+
+        post_dict = post.model_dump()
+        post_dict["id"] = id
+        print(post_dict)
+        for index, entry in enumerate(output):
+            if entry.get("id") == id:
+                output[index] = post_dict
+
+
+
+        with open(FILE_PATH, "w") as file:
+            json.dump(output, file, indent=4)
+
+        return {"Message":"post updated"}
+
+    except FileNotFoundError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=
+                            "This file does not exist")
